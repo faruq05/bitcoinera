e96532b3e9cb3c391eb447a9d9bf8f37545b6282
Omor Faruq <faruq05.fiverr@gmail.com>
2023-02-01 16:04:41 +0600

    Done

--- js/accordian_icon.js
+++ js/accordian_icon.js
@@ -1,3 +1,31 @@
-$(".rotate").click(function () {
-    $(this).toggleClass("down");
-})
+$("#headingOne").click(function () {
+    $(".rotate").toggleClass("rotated");
+});
+
+$("#headingTwo").click(function () {
+    $(".rotate").toggleClass("rotated");
+});
+
+$("#headingThree").click(function () {
+    $(".rotate").toggleClass("rotated");
+});
+
+$("#headingFour").click(function () {
+    $(".rotate").toggleClass("rotated");
+});
+
+$("#headingFive").click(function () {
+    $(".rotate").toggleClass("rotated");
+});
+
+$("#headingSix").click(function () {
+    $(".rotate").toggleClass("rotated");
+});
+
+$("#headingSeven").click(function () {
+    $(".rotate").toggleClass("rotated");
+});
+
+$("#headingEight").click(function () {
+    $(".rotate").toggleClass("rotated");
+});
